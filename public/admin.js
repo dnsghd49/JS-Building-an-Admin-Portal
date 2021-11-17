3be9d1b3d200be8ed2368df38eb7ae527ce5856b
Aaron Lu <aaronnoh0409@gmail.com>
2021-11-17 16:52:31 -0500

    fetching api

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,6 @@
-
-// Your Code Here
+const books_url = "http://localhost:3001/listBooks"
+async function getBooks() {
+    let response = await fetch(books_url)
+    let data = await response.json()
+    console.log(data)
+}
